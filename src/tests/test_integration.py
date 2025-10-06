a021894b69770c70df76fca7cfedc2bfa9336069
Platon7002 <169038426+Platon7002@users.noreply.github.com>
2025-10-06 03:53:46 +0300

    Update test_integration.py

--- src/tests/test_integration.py
+++ src/tests/test_integration.py
@@ -8,20 +8,20 @@ class IntegrationTest(unittest.TestCase):
 
     def test_function_printing(self):
         with patch('sys.stdout', new=StringIO()) as fake_out:
-            start(list(["get_tpl", "--f_login=george.sagomonmov@gmail.com", "--f_tel=+7 903 123 45 78"]))
+            start(list(["get_tpl", "--f_login=platonlap@mail.ru", "--f_tel=+7 903 123 45 78"]))
             self.assertEqual("Данные пользователя", fake_out.getvalue().strip())
 
     def test_fail_command(self):
         with self.assertRaisesRegex(ValueError, r"Invalid command"):
-            start(list(["got_tpl", "--f_login=george.sagomonmov@gmail.com", "--f_tel=+7 903 123 45 78"]))
+            start(list(["got_tpl", "--f_login=platonlap@mail.ru", "--f_tel=+7 903 123 45 78"]))
 
     def test_fail_fields(self):
      with self.assertRaisesRegex(ValueError, r"No fields provided"):
-            start(list(["get_tpl", "--login=george.sagomonmov@gmail.com", "--tel=+7 903 123 45 78"]))
+            start(list(["get_tpl", "--login=platonlap@mail.ru", "--tel=+7 903 123 45 78"]))
 
     def test_template_not_found(self):
         with patch('sys.stdout', new=StringIO()) as fake_out:
-            start(list(["get_tpl", "--f_login=george.sagomonmov@gmail.com", "--f_tsl=+7 903 123 45 78"]))
+            start(list(["get_tpl", "--f_login=platonlap@mail.ru", "--f_tsl=+7 903 123 45 78"]))
             self.assertEqual("No matching templates found", fake_out.getvalue().strip())
 
 if __name__ == '__main__':
